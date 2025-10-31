f649702927e170c8f57b24e11ca6c35529ed78b5
bonaxl015 <bonaxl015@gmail.com>
2025-10-31 21:54:56 +0800

    feat(register): user registration service, controller and module

---
+++ src/users/users.service.ts
@@ -0,0 +1,39 @@
+import { Injectable, ConflictException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UsersService {
+	constructor(private readonly prisma: PrismaService) {}
+
+	async createUser(data: CreateUserDto) {
+		const existingUser = await this.prisma.user.findUnique({
+			where: { email: data.email }
+		});
+
+		if (existingUser) {
+			throw new ConflictException('Email already registered');
+		}
+
+		const hashedPassword = await bcrypt.hash(data.password, 12);
+
+		const user: Omit<User, 'password'> = await this.prisma.user.create({
+			data: {
+				email: data.email,
+				name: data.name,
+				password: hashedPassword
+			},
+			select: {
+				id: true,
+				email: true,
+				name: true,
+				createdAt: true,
+				updatedAt: true
+			}
+		});
+
+		return user;
+	}
+}

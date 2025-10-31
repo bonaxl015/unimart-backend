d706b6c347e386bd54358a25d05fc0c548722097
bonaxl015 <bonaxl015@gmail.com>
2025-10-31 22:50:24 +0800

    feat(login): login module, controller and service setup

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,15 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto, loginSchema } from './dto/login.dto';
+
+@Controller('auth')
+export class AuthController {
+	constructor(private readonly authService: AuthService) {}
+
+	@Post('login')
+	async login(@Body() body: unknown) {
+		const parsedData: LoginDto = loginSchema.parse(body);
+
+		return this.authService.login(parsedData);
+	}
+}

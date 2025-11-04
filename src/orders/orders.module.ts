1f069a73672d9210c1a34415b8d37eaa100fdd81
Bon Axl Feeser <78939695+bonaxl015@users.noreply.github.com>
2025-11-04 19:15:06 +0800

    feat(payments): setting up payment feature using stripe (#13)

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
+import { PaymentsService } from '../payments/payments.service';
 
 @Module({
-	providers: [OrdersService],
+	providers: [OrdersService, PaymentsService],
 	controllers: [OrdersController]
 })
 export class OrdersModule {}
---
+++ src/orders/utils/order-processor.ts
@@ -0,0 +1,50 @@
+import {
+	CartItem,
+	OrderStatus,
+	PaymentStatus,
+	Prisma,
+	PrismaClient,
+	Product
+} from '@prisma/client';
+import { PrismaService } from '../../prisma/prisma.service';
+import { ITXClientDenyList } from '@prisma/client/runtime/binary';
+
+export class OrderProcessor {
+	constructor(private readonly prisma: PrismaService) {}
+
+	async createOrder(
+		userId: string,
+		total: number,
+		cartItems: (CartItem & { product: Product })[],
+		tx: Omit<PrismaClient, ITXClientDenyList>
+	) {
+		return await tx.order.create({
+			data: {
+				userId: userId,
+				total: new Prisma.Decimal(total),
+				paymentStatus: PaymentStatus.PENDING,
+				status: OrderStatus.PENDING,
+				items: {
+					create: cartItems.map((item) => ({
+						productId: item.productId,
+						quantity: item.quantity,
+						price: new Prisma.Decimal(item.product.price)
+					}))
+				}
+			}
+		});
+	}
+
+	async attachPaymentIntent(
+		orderId: string,
+		intentId: string,
+		tx: Omit<PrismaClient, ITXClientDenyList>
+	) {
+		return await tx.order.update({
+			where: { id: orderId },
+			data: {
+				paymentIntentId: intentId
+			}
+		});
+	}
+}
